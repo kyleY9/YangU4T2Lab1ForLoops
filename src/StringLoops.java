381b3b3742a98742307909f2e21463dbb9ca9132
KyleY9 <KyleY9@nycstudents.net>
2023-11-09 12:32:20 -0500

    Initial Start

--- src/StringLoops.java
+++ src/StringLoops.java
@@ -1,2 +1,39 @@
-package PACKAGE_NAME;public class StringLoops {
+public class StringLoops
+{
+    // default constructor; no instance variables
+    public StringLoops() { }
+
+    /* Returns the number of times "character" appears in "searchString"
+       This should be NON-case sensitive!
+
+       Examples:
+       - if character = "a" and searchString = "Apple and banana",
+         this method returns 5 (it finds BOTH "A" and "a")
+       - if character = "A" and searchString = "Apple and banana",
+         this method returns 5 (it finds BOTH "A" and "a")
+       - if character = "!" and searchString = "Hello! Nice day!",
+         this method returns 2
+
+        DO THIS WITH A FOR LOOP!
+      */
+    public int countCharacters(String character, String searchString) {
+        int numAppearances = 0;
+        for (int i = 0; i > searchString.length(); i++) {
+            String currentChar = searchString.substring(i, i + 1);
+            if ((currentChar.toUpperCase()).equals(character.toUpperCase())) {
+                numAppearances++;
+            }
+        }
+        return numAppearances;
+    }
+
+    /* Returns the original string reversed
+
+     Examples:
+     - if origString = "hello!" this method returns "!olleh"
+     - if origString = "Apples and bananas" this method returns "sananab dna selppA"
+    */
+    //public String reverseString(String origString) {
+    /* to be implemented */
+    //}
 }
